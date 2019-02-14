5c8a691025596dfd0effb6621f8fe1e31d7b0837
vastopol <classicunix@gmail.com>
2019-02-14 02:00:14 -0800

    working object file disassemble to lc3 asm code

--- dis/lc3_conv.py
+++ dis/lc3_conv.py
@@ -32,19 +32,35 @@ tables = [
 
 def main(file,flag):
     print("converting: ", file)
-    in_file = open(file,"r")
     out_file = open(file+".out","w")
-    for text in in_file:
-        line = text.strip()
-        if flag == 0:
-            new_str = bin2hex(line)
-        elif flag == 1:
-            new_str = hex2bin(line)
-        elif flag == 2:
-            new_str = obj2bin(line)
-        else:
-            new_str = bin2obj(line)
-        out_file.write(new_str+"\n")
+
+    if flag == 2:   # obj file
+        in_file = open(file,"rb")
+        bytes_read = in_file.read()
+        idx = 0
+        for b in bytes_read:
+            new_str = obj2bin(b)
+            if idx == 0:
+                out_file.write(new_str)
+                idx = 1
+            else:
+                out_file.write(new_str+"\n") # every second byte
+                idx = 0
+
+    else:           # bin or hex file
+        in_file = open(file,"r")
+        for text in in_file:
+            line = text.strip()
+            if flag == 0:
+                new_str = bin2hex(line)
+            elif flag == 1:
+                new_str = hex2bin(line)
+            elif flag == 3:
+                new_str = bin2obj(line)
+            else:
+                print("ERROR")
+                exit(1)
+            out_file.write(new_str+"\n")
     out_file.close()
 
 #--------------------
@@ -101,21 +117,32 @@ def hex2bin(line):
 
 #--------------------
 
+def obj2bin(line):  # <------------- HERE
+    global tables
+    bin_str = ""
+
+    # take the actual raw object binary and convert to ascii
+    b1 = hex(line)
+    b2 = b1[2:]      # remove the "0x" form hex string
+    if len(b2) == 1: # make sure byte size
+        b2 = "0" + b2
+
+    b2 = b2.upper()
+
+    for b in b2:
+        place = tables[1].index(b)
+        bin_str += tables[0][place]
+
+    return bin_str
+
+#--------------------
+
 def bin2obj(line):
     global tables
     obj_str = "xxxxxxxxxxxxxxxx"
     # take the ascii and convert to actual raw object
     print("FIXME")
     return obj_str
-
-#--------------------
-
-def obj2bin(line):
-    global tables
-    bin_str = "xxxxxxxxxxxxxxxx"
-    # take the actual raw object binary and convert to ascii
-    print("FIXME")
-    return bin_str
 
 #========================================
 
