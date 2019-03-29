c62e0dc4c199a7b9b713c148b372f71757853c02
vastopol <classicunix@gmail.com>
2019-03-28 19:02:54 -0700

    progress, try symbolize next

---
+++ dis/scripts/generate_str.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+# https://stackoverflow.com/questions/43119744/python-generate-all-possible-strings-of-length-n
+
+import string
+import itertools
+
+def generate_strings(str,len):
+    for item in itertools.product(str, repeat=len):
+        yield "".join(item)
+
+
+bins = "01"
+bstr = generate_strings(bins,16)
+
+hexs = "0123456789ABCDEF"
+hstr = generate_strings(hexs,4)
+
+for b in bstr:
+    print(b)
+
+for h in hstr:
+    print(h)
